bbbe0041191cd749326eb1710d8f3fa9a8cbbbd1
Nikolina <niikolina.kircanski@gmail.com>
2023-05-02 20:43:22 +0200

    add0205

--- src/pages/vjezba0205.tsx
+++ src/pages/vjezba0205.tsx
@@ -5,19 +5,49 @@ type MyData = {
 const Vjezba0205=()=>{
     const mojaVjezba:MyData[]=[
        { 
-        myName="Nikolina",
-        myAge=40,
+        myName:"Nikolina",
+        myAge:40,
     },
     ];
-    
-   
+//zadatak2
+   let mojeGodine=mojaVjezba[0].myAge;
+   for(let i=0; i < mojaVjezba.length; i++){
+    if(mojaVjezba[i].myAge < mojeGodine){
+        mojeGodine=mojaVjezba[i].myAge;
+    };
+   };
+console.log(`nemam više od`+mojeGodine);
 
-   
-   
-   
+//zadatak3
+const myName="Nikolina"
+console.log(myName.length);
+
+//zadatak4
+ let numberList=[2,3,5,8,12,19,25,65,52,11];  
+  for ( let i = 0; i < numberList.length; i++);
+  console.log(numberList);
+  
+  
+  
+
+
     return(
         <div className="container">
             <h1>Vjezba 0205</h1>
+            <hr />
+            <div>
+                {
+                    mojaVjezba.map((podaci)=>{
+                        return(
+                            <div>
+                                
+                            {podaci.myName} {podaci.myAge}
+                            </div>
+
+                        );
+                    })}
+                
+            </div>
         </div>
     );
 };
