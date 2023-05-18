885eb94a4ed67f3f2be1d9c594080c792f434c7e
Nikolina <niikolina.kircanski@gmail.com>
2023-05-18 14:58:27 +0200

    my

--- src/pages/vjezba1510.tsx
+++ src/pages/vjezba1510.tsx
@@ -45,24 +45,56 @@ const Vjezba1505 = () => {
 
   //6. Funkcija koja pretvara celsius to fahrenheit
   //formula =>  °C * 1.8 + 32 = °F
-  const celsiusToFahrenheit = (celsius: string) => {
+  const celsiusToFahrenheit = (celsius: number) => {
     return celsius * (1.8 + 32);
   };
 
   //7. Funkcija koja ispisuje string naopako
-  const reverseString = (str: boolean) => {
+  const reverseString = (str: any) => {
     let reversedStr = "";
     for (let i = str.length - 1; i > 0; i--) {
       reversedStr += str[i - 1];
     }
     return reversedStr;
   };
+  //8.Funkcija kreira palindrom što ne valja?
+  const isPalindrome = (str: string) => {
+    const reversedStr = str.split("").reverse().join("");
+    return str === reversedStr;
+  };
+
+  //9. Funkcija kreira fibonacciev niz ispravi pogresku:
+  const fibonacciSequence = (n:number) => {
+    if (n <= 0) {
+      return [];
+    } else if (n === 1) {
+      return [1];
+    } else if (n === 2) {
+      return [1, 1];
+    } else {
+      const sequence = [1, 1];
+      for (let i = 3; i <= n; i++) {
+        sequence.push(sequence[i] + sequence[i - 1]);
+      }
+      return sequence;
+    }
+  };
+
+  //10. Funkcjia filtrira sve parne brojeve iz array-a ispravi gresku
+  const filterEvenNumbers = (arr:number[]) => {
+    return arr.filter((num) => {
+      if (num % 2 === 0) {
+        return num;
+      }
+    });
+  };
 
   useEffect(() => {
+    console.log(filterEvenNumbers([4,3,2]));
     //console.log(add(1, 3));
     //console.log(countToTen());
     //console.log(getFullName(user));
-    console.log(sumArray([1, 2, 3, 5]));
+    // console.log(sumArray([1, 2, 3, 5]));
     //Ovdje možemo pokretati funkciju koju testiramo
   }, []);
   return <div className="container"></div>;
