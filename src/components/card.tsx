d033f89b487972cea2d0cee9a28dd5e7c8c96a68
grafika10 <grafika10@ucione.local>
2023-05-22 20:13:50 +0200

    dddd

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -18,7 +18,7 @@ const Card = () => {
         1500s, when an unknown printer took a galley of type and scrambled it to
         make a type specimen book. It has survived not only five centuries, but
         also the leap into electronic typesetting, remaining essentially
-        unchanged.
+        unchangeddd.
       </p>
       <div className="card__btn">
         <Button />
