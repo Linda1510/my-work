fd69922dd0eac310dab58c61c47ce734695007bb
grafika10 <grafika10@ucione.local>
2023-05-22 20:15:07 +0200

    book

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -16,8 +16,8 @@ const Card = () => {
       <p className="card__desc">
         Lorem Ipsum has beenn the industry's standard dummy text ever since the
         1500s, when an unknown printer took a galley of type and scrambled it to
-        make a type specimen book. It has survived not only five centuries, but
-        also the leap into electronic typesetting, remaining essentially
+        make a type specimen booook. It has survived not only five centuries,
+        but also the leap into electronic typesetting, remaining essentially
         unchangeddd.
       </p>
       <div className="card__btn">
