159b6fa04524cc0ca05acee3ecbd9ced657c75e2
grafika10 <grafika10@ucione.local>
2023-05-22 20:19:41 +0200

    unknown

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -15,10 +15,10 @@ const Card = () => {
 
       <p className="card__desc">
         Lorem Ipsum has beenn the industry's standard dummyy text ever since the
-        1500s, when an unknown printer took a galley of type and scrambled it to
-        make a type specimenn booooook. It has survived not only five centuries,
-        but also theeee leap into electronic typesetting, remaining essentially
-        unchanged.
+        1500s, when an unknownn printer took a galley of type and scrambled it
+        to make a type specimenn booooook. It has survived not only five
+        centuries, but also theeee leap into electronic typesetting, remaining
+        essentially unchanged.
       </p>
       <div className="card__btn">
         <Button />
