d2139aa0b11ab9296b4574548f87bb7d216fdf56
grafika10 <grafika10@ucione.local>
2023-05-22 20:13:42 +0200

    dd

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -46,8 +46,8 @@ const Header = () => {
           <a className="header__nav__item " href="/pokemon">
             Pokemon
           </a>
-          <a className="header__nav__item klasa " href="/europe">
-            Europee
+          <a className="header__nav__item  " href="/europe">
+            Europe
           </a>
           <a className="header__nav__item " href="/africa">
             Africa
