ef25b0088dd155b034fa55ba2f3c75b343d449e6
grafika10 <grafika10@ucione.local>
2023-05-22 20:17:33 +0200

    comit

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -14,7 +14,7 @@ const Card = () => {
       <h1 className="card__title">Naslovvvv</h1>
 
       <p className="card__desc">
-        Lorem Ipsum has beenn the industry's standard dummy text ever since the
+        Lorem Ipsum has beenn the industry's standard dummyy text ever since the
         1500s, when an unknown printer took a galley of type and scrambled it to
         make a type specimen booook. It has survived not only five centuries,
         but also the leap into electronic typesetting, remaining essentially
